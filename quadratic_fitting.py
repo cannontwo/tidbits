97fb8c13202dae27743f8c5e27df7d476885fe15
Cannon Lewis <cannon10100@gmail.com>
2019-07-15 17:27:45 -0500

    Added sklearn fitting of the quadratic model. Today I've learned that the sklearn linear regression library is better than my hand implementation.

--- quadratic_fitting.py
+++ quadratic_fitting.py
@@ -1,5 +1,9 @@
 import numpy as np
+
+from itertools import chain, combinations_with_replacement
+
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
 
 def fit_quadratic_model(state_dim, states, targets):
     """
@@ -37,6 +41,15 @@ def fit_quadratic_model(state_dim, states, targets):
     V_0 = theta[0, -1].copy()
 
     return (V_0, V_x, V_xx)
+
+def sk_fit_quadratic_model(states, targets):
+    poly = PolynomialFeatures(degree=2)
+    X_ = poly.fit_transform(states)
+
+    clf = linear_model.LinearRegression()
+    clf.fit(X_, targets)
+
+    return clf
 
 def generate_random_data(dim=3, size=50):
     V_0 = np.random.randn(1, 1)
@@ -87,27 +100,79 @@ def get_accuracy(states, targets, V_0_hat, V_x_hat, V_xx_hat):
     avg_error = total_error / float(len(targets))
     return avg_error
 
+def sk_get_accuracy(states, targets, model):
+    total_error = 0.0
+
+    for state, target in zip(states, targets):
+        state = state.reshape((1, -1))
+        poly = PolynomialFeatures(degree=2)
+
+        estimate = model.predict(poly.fit_transform(state))
+
+        total_error += (target - estimate) ** 2
+
+    avg_error = total_error / float(len(targets))
+    return avg_error
+
+def get_mats_from_sk(model, dim=3):
+    # This is the ordering that PolynomialFeatures uses
+    powers = list(chain.from_iterable(combinations_with_replacement(range(dim), i) for i in range(3)))
+    coefs = model.coef_
+
+    V_0 = np.zeros((1, 1))
+    V_x = np.zeros((dim, 1))
+    V_xx = np.zeros((dim, dim))
+
+    for idx, power in enumerate(powers):
+        if len(power) == 0:
+            V_0 = coefs[idx]
+        elif len(power) == 1:
+            V_x[power] += coefs[idx]
+        elif len(power) == 2:
+            V_xx[power[0], power[1]] += coefs[idx] / 2.0
+            V_xx[power[1], power[0]] += coefs[idx] / 2.0
+
+    return (V_0, V_x, V_xx)
+
 if __name__ == "__main__":
     for i in range(10):
         print("Accuracy of random estimate:")
         r_V_0 = np.random.randn(1, 1)
-        r_V_x = np.random.randn(6, 1)
-        r_V_xx = np.random.randn(6, 6)
-        states, targets, V_0, V_x, V_xx = generate_random_data(dim=6, size=100)
+        r_V_x = np.random.randn(3, 1)
+        r_V_xx = np.random.randn(3, 3)
+        states, targets, V_0, V_x, V_xx = generate_random_data(dim=3, size=100)
         acc = get_accuracy(states, targets, r_V_0, r_V_x, r_V_xx) 
         print("Error of random least squares estimate was {}".format(acc))
 
     for size in [1, 5, 10, 100, 500]:
         print("\n{} data points:".format(size))
         total_acc = 0.0
+        sk_total_acc = 0.0
+        reconstructed_sk_total_acc = 0.0
 
         for _ in range(10):
-            states, targets, V_0, V_x, V_xx = generate_random_data(dim=6, size=size)
-            V_0_hat, V_x_hat, V_xx_hat = fit_quadratic_model(6, states, targets)
+            states, targets, V_0, V_x, V_xx = generate_random_data(dim=3, size=size)
+
+            V_0_hat, V_x_hat, V_xx_hat = fit_quadratic_model(3, states, targets)
+            sk_model = sk_fit_quadratic_model(states, np.array(targets).flatten())
             
-            more_states, more_targets = generate_more_data(V_0, V_x, V_xx, dim=6)
+            more_states, more_targets = generate_more_data(V_0, V_x, V_xx, dim=3)
+
             acc = get_accuracy(more_states, more_targets, V_0_hat, V_x_hat, V_xx_hat)
             total_acc += acc
+            
+            sk_acc = sk_get_accuracy(more_states, more_targets, sk_model)
+            sk_total_acc += sk_acc
+
+            sk_V_0, sk_V_x, sk_V_xx = get_mats_from_sk(sk_model, dim=3)
+
+            reconstructed_acc = get_accuracy(more_states, more_targets, sk_V_0, sk_V_x, sk_V_xx)
+            reconstructed_sk_total_acc += reconstructed_acc
 
         avg_acc = total_acc / 10.0
+        avg_sk_acc = sk_total_acc / 10.0
+        avg_reconstructed_acc = reconstructed_sk_total_acc / 10.0
+
         print("Average error of least squares estimate was {}".format(avg_acc))
+        print("Average error of sklearn estimate was {}".format(avg_sk_acc))
+        print("Average reconstructed error of sklearn estimate was {}".format(avg_reconstructed_acc))
